02f81cf1aebe2026cca9f7ac1a98821a5eda78b7
dudals3844 <44514383+dudals3844@users.noreply.github.com>
2020-08-25 15:09:50 +0900

    rsi 상승폭이 계속 0일때 오류발생

--- test3/rsi.py
+++ test3/rsi.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sqlite3
 from test3.etl import *
 import datetime
+import numpy as np
 
 class DataBase(DataBase):
     def readToDataFrame(self, tableName):
@@ -21,17 +22,72 @@ class QuaterTime:
         else:
             return False
 
-class QuaterClosePrice:
+class RSI:
+    def __init__(self):
+        self.upList = []
+        self.downList = []
+
+
+    def calculate(self, indexRangeList, dataFrame):
+        rsiList = []
+        startIndex = 0
+        for endIndex in indexRangeList:
+            endIndex = int(endIndex)
+            for dataFrameIndex in range(startIndex, endIndex-2):
+                medoHoga = dataFrame['매도호가'].iloc[dataFrameIndex]
+                afterMedoHoga = dataFrame['매도호가'].iloc[dataFrameIndex]
+                self.upORDown(medoHoga=medoHoga, afterMedoHoga= afterMedoHoga)
+
+            upMean, downMean = self.upORDownAverage()
+            self.rsivalue = upMean/(upMean + downMean)
+            rsiList.append(self.rsivalue)
+            print(self.rsivalue)
+            self.upList = []
+            self.downList = []
+            startIndex = endIndex
+
+    def upORDown(self, medoHoga, afterMedoHoga):
+        medoHoga = float(medoHoga)
+        afterMedoHoga = float(afterMedoHoga)
+        if medoHoga >= afterMedoHoga:
+            spread = medoHoga - afterMedoHoga
+            self.upList.append(spread)
+
+        elif medoHoga <= afterMedoHoga:
+            spread = afterMedoHoga - medoHoga
+            self.downList.append(spread)
+
+    def upORDownAverage(self):
+        upMean = np.mean(self.upList)
+
+        if len(self.downList) != 0:
+            downMean = np.mean(self.downList)
+        else:
+            downMean = 0
+
+
+        return upMean, downMean
+
+
+
+
+
+
+class QuaterClosePriceIndex:
     def __init__(self):
         self.tmp = True
-        self.isQuaterTime = QuaterTime()
+        self.QuaterTime = QuaterTime()
 
     def calculate(self, datatFrame):
+        indexRangeList = []
         for i in range(len(datatFrame)):
             time = datatFrame['거래시간'].iloc[i]
-            medoHoga = datatFrame['매도호가'].iloc[i]
-            if self.isQuaterTime.isQuaterTimer(time) != self.tmp and self.isQuaterTime.isQuaterTimer(time) == True:
-                pass
+            if self.QuaterTime.isQuaterTimer(time) != self.tmp and self.QuaterTime.isQuaterTimer(time) == True:
+                i = str(i)
+                indexRangeList.append(i)
+
+            self.tmp = self.QuaterTime.isQuaterTimer(time)
+        return indexRangeList
 
 if __name__ == '__main__':
     csvName = CSVFileName()
@@ -48,3 +104,8 @@ if __name__ == '__main__':
     time = df['거래시간'].iloc[0]
     quaterTime = QuaterTime()
     print(quaterTime.isQuaterTimer(time))
+
+    quaterCloseIndex = QuaterClosePriceIndex()
+    indexRangeList = quaterCloseIndex.calculate(datatFrame=df)
+    rsi = RSI()
+    rsi.calculate(indexRangeList=indexRangeList, dataFrame=df)
